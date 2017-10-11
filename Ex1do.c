6e7febe9da9ff6140988eb848bfa780a635aeba0
Skander Blaity <32510139+skandrblaity@users.noreply.github.com>
2017-10-11 12:34:35 +0100

    Add files via upload

---
+++ Ex1do.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int f,i,r;
+	printf("Saisissez un entier: ");
+	scanf("%d",&f);
+	i=f;
+	r=f;
+	do
+		{
+		   i=i-1;
+		   r=r*i;
+		}while (i>1);
+	printf("La factorielle de %d = %d",f,r);
+	return 0;
+}
